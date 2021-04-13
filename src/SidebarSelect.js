868bee964cba5ad591ef36fa08d1665fa00dd3e8
ttheolier <theolier@usc.edu>
2021-04-12 20:36:00 -0700

    Finally got those stupid buttons on the left to flex

--- src/SidebarSelect.js
+++ src/SidebarSelect.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 function SidebarSelect({Icon,text}){
     return(
         <div className = "sidebarSelect">
-            <Button size = "large" target="_blank" href={text} startIcon = {<Icon/>}>{text}</Button>
+            <Button size = "large" target="_blank" href={text} startIcon = {<Icon/>} className = "sideOption" style={{justifyContent: "flex-start"}} fullWidth = {true}>{text}</Button>
         </div>
     )
 }
