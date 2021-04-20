17763cb550dac7d37f98ef33f1d125d9d6820c39
ttheolier <theolier@usc.edu>
2021-04-19 23:14:14 -0700

    login page ui done, no functionality

--- src/DailySpark.js
+++ src/DailySpark.js
@@ -4,13 +4,28 @@ import DailyBox from './DailyBox';
 import db from './firebase';
 
 function DailySpark() {
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+        db.collection("posts").onSnapshot((snapshot)=> 
+        setPosts(snapshot.docs.map((doc)=>doc.data()))
+        );
+    }, []);
     return(
         <div className = "dailySpark">
             <div className = "dsHeader">
                 <h1>Daily Spark</h1>
             </div>
             <div className = "dsSpark">
-                <DailyBox text = "this is a palceholder that is intentionally long enough to fill up mulitple rows of text to test flex"/>
+                {/*only getting the first, not a random one */}
+                {posts.slice(0,1).map(post =>(
+                    <DailyBox
+                    text = {post.text}
+                    avat = {post.avatar}
+                    date = {post.date}
+                    username = {post.username}
+                    />
+                ))}
             </div>
         </div>
     );
--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,18 @@ function Login(){
                 <a href = "Login">Login</a>
                 <a href = "Register">Register</a>
             </div>
+            <div className = "fields">
+                <p>Email</p>
+                <input type="text" id="email" name="email"></input><br/>
+                <p>Password</p>
+                <input type="text" id="password" name="password"></input><br/>
+            </div>
+            <div className = "subButtons">
+                <Button variant = "contained">guest</Button>
+                <div className = "loginButton">
+                    <Button variant = "contained" color = "powderblue">Login</Button>
+                </div>
+            </div>
             
         </div>
     )
